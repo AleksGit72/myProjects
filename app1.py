3248dca20d6d9700e096292264ae5251a5bde9ca
och <och@ektos.net>
2023-11-01 14:23:10 +0200

    100001

---
+++ app1.py
@@ -0,0 +1,4 @@
+prompt = "This is a simple question to know what you like."
+prompt += "\nSo, please say your favorite place: "
+example = input(prompt)
+print(example + " is a great place to visit")
