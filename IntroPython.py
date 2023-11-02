d211effe95fd5ff204ecf62a9fcd7b21bbfc1dbc
och <och@ektos.net>
2023-11-02 09:59:57 +0200

    cmt0002

--- IntroPython.py
+++ IntroPython.py
@@ -1,3 +1,6 @@
+Xx1=int (input("Я жду число1:"))
+xX2=int (input ("И ещё число2:"))
+print (Xx1+xX2)
 first = float(input("First: "))
 second = float(input("Second: "))
 sum = first + second
---
+++ list.py
@@ -0,0 +1,9 @@
+sample = ['Nevada', 'Ohio', 'Colorado']
+print(sample)
+print(sample[0])
+print(sample[1])
+print(sample[2])
+print(sample[2] + ' is a great city')
+print(sample[-1])
+x = [[1, 223], [245, 63, 22]]
+print(x[1][2])
