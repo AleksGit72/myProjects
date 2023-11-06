241c5d2e8b81f014ac681891f5d3720e97b16943
och <och@ektos.net>
2023-11-06 17:57:45 +0200

    1

--- global.py
+++ global.py
@@ -7,3 +7,16 @@ def second_sample():
     print(example)
 sample()
 second_sample()
+num = int(input())
+first_digit = num % 10
+second_digit = (num % 100) // 10
+third_digit = num // 100
+c = str(first_digit)
+b = str(second_digit)
+a = str(third_digit)
+print(int(a+b+c))
+print(int(a+c+b))
+print(int(b+a+c))
+print(int(b+c+a))
+print(int(c+a+b))
+print(int(c+b+a))
