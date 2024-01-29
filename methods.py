a632c58ea9265a7b2283792c02396e51d64a5cdc
och <och@ektos.net>
2024-01-29 16:25:04 +0200

    1

--- methods.py
+++ methods.py
@@ -9,7 +9,7 @@ print('YES' if s == s.title() else 'NO')
 
 print('YES' if input().lower().find('хорош') != -1 else 'NO')
 
-"""
+
 # подсчитывает количество буквенных символов в нижнем регистре.
 s, count = input(), 0
 for i in s:
@@ -18,3 +18,28 @@ for i in s:
 print(count)
 
 
+s = input().lower()
+print('Аденин: ' + str(s.count('а')), 'Гуанин: ' + str(s.count('г')), 'Цитозин: '+ str(s.count('ц')), 'Тимин: ' + str(s.count('т')), sep = '\n')
+
+
+n, count = int(input()), 0
+for _ in range(n):
+    if input().count('11') >= 3:
+        count +=1
+print(count)
+
+# подсчитывает количество цифр в строке.
+s, count = input(), 0
+for i in s:
+    if i in '0123456789':
+        count += 1
+print(count)
+
+# подсчитывает количество цифр в строке.
+s, total = input(), 0
+for i in range(10):
+    total += s.count(str(i))
+print(total)
+"""
+s = input()
+print('YES' if s.endswith(('.com', '.ru')) else 'NO')
