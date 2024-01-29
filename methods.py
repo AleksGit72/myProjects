e9ee4eadd96d255f5249bfdc8e07a56291ffd7cf
och <och@ektos.net>
2024-01-29 11:46:47 +0200

    1

--- methods.py
+++ methods.py
@@ -1,2 +1,20 @@
+"""
 s = 'i Learn Python language'
 print(s.capitalize())
+
+
+s = input()
+print('YES' if s == s.title() else 'NO')
+
+
+print('YES' if input().lower().find('хорош') != -1 else 'NO')
+
+"""
+# подсчитывает количество буквенных символов в нижнем регистре.
+s, count = input(), 0
+for i in s:
+    if i != i.upper():
+        count += 1
+print(count)
+
+
