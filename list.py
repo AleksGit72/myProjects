7b978491278e4da345ed672b7ae7abc9c80f7a41
och <och@ektos.net>
2024-02-13 14:40:51 +0200

    1

--- list.py
+++ list.py
@@ -311,6 +311,8 @@ numbers.insert(3, 25)
 print(numbers)
 """
 lst = input().split()
+# print(lst)
 ind_min, ind_max = lst.index(min(lst)), lst.index(max(lst))
 lst[ind_min], lst[ind_max] = lst[ind_max], lst[ind_min]
+# print(lst)
 print(*lst)
