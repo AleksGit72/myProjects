d0acf32c0e771445b33f9c72836b8784a57ef3d7
OCH <och@ektos.net>
2024-04-25 15:44:41 +0300

    1

--- lesson2_4_step8.py
+++ lesson2_4_step8.py
@@ -15,15 +15,19 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    # говорим Selenium проверять в течение 2 секунд, пока кнопка не станет кликабельной
+    # говорим Selenium ждать в течение 15 секунд, пока появится нужное значение
     button = browser.find_element(By.ID, "book")
     WebDriverWait(browser, 15).until(ec.text_to_be_present_in_element((By.ID, "price"), "100"))
     button.click()
 
-    browser.execute_script("window.scrollBy(0, 300);", button)  # scrolling by 300 px down
+    # найти кнопку подтверждения решения
+    submit_button = browser.find_element (By.ID, 'solve')
+
+    # проскроллить до кнопки подтверждения решения
+    browser.execute_script('return arguments[0].scrollIntoView(true);', submit_button)
 
     #  Считать значение для переменной var:
-    x_element = browser.find_element('xpath', '//*[@id="input_value"]')
+    x_element = browser.find_element(By.ID, "input_value")
     x = x_element.text
     y = calc(x)
 
@@ -31,10 +35,14 @@ try:
     browser.find_element(By.ID, "answer").send_keys(y)
 
     #  подтвердить ввод решения.
-    browser.find_element(By.ID, "solve").click()
+    submit_button.click()
+
 
 finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
+
+    print(browser.switch_to.alert.text.split()[-1])
+    #  ожидание чтобы визуально оценить результаты прохождения скрипта
+    #  time.sleep(10)
+
+    #  закрываем браузер после всех манипуляций
     browser.quit()
